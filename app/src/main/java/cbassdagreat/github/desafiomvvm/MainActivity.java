4e15ba911e0fcd3b1d3d106d036b04726e19c960
Cbass <claveria.sebastian@gmail.com>
2022-03-11 21:19:33 -0300

    se sigue avanzando. falta el cont. y problemas con el binding y el método de length, hasNum, hasCaps

--- app/src/main/java/cbassdagreat/github/desafiomvvm/MainActivity.java
+++ app/src/main/java/cbassdagreat/github/desafiomvvm/MainActivity.java
@@ -3,7 +3,10 @@ package cbassdagreat.github.desafiomvvm;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import cbassdagreat.github.desafiomvvm.databinding.ActivityMainBinding;
 import cbassdagreat.github.desafiomvvm.viewmodel.PasswordViewModel;
@@ -12,21 +15,68 @@ public class MainActivity extends AppCompatActivity {
 
     private PasswordViewModel viewModel;
     private ActivityMainBinding binding;
+    private int cont = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         viewModel = new ViewModelProvider(this).get(PasswordViewModel.class);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         binding.setLifecycleOwner(this);
+
 
         String password = binding.etPass.getText().toString();
         //viewModel.
 
+        binding.etPass.addTextChangedListener((new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String pass = s.toString();
+                //viewModel.hasCaps(pass, binding.etPass, g);
+                //viewModel.hasLength(pass, binding.etPass,;
+                //viewModel.hasNum(pass,binding.etPass,getContext());
+
+
+
+            }
+        }));
 
 
     }
+
+    public void security(int cont) {
+        if (cont == 0) {
+            binding.tvPassStrength.setText(R.string.debil);
+            binding.tvPassStrength.setBackgroundColor(Color.RED);
+
+        }else if (cont == 1){
+            binding.tvPassStrength.setText(R.string.media);
+            binding.tvPassStrength.setBackgroundColor(Color.MAGENTA);
+
+        }else if (cont == 2) {
+            binding.tvPassStrength.setText(R.string.fuerte);
+            binding.tvPassStrength.setBackgroundColor(Color.YELLOW);
+        }
+        else {
+            binding.tvPassStrength.setText(R.string.super_fuerte);
+            binding.tvPassStrength.setBackgroundColor(Color.GREEN);
+        }
+
+    }
+
 
 
 }
